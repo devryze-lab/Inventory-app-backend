926e3c80748711a3bddd81b682f9155b534b243a
Devryze <devryze.info@gmail.com>
2025-04-29 00:00:58 +0500

    Update server.js
    allowing all domains on cors

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const allowedOrigin = 'http://localhost:5173/';
-app.use(cors(
-  origin: 'http://localhost:5173', // your frontend origin
-  credentials: true // if you need to send cookies/auth
-));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
