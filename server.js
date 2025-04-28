47e27579d141f2d8dcff0461623937a22fc07a80
Devryze <devryze.info@gmail.com>
2025-04-28 23:38:57 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
