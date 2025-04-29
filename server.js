7e86f3eb924f3b2e2c5d0394643644770c475423
Devryze <devryze.info@gmail.com>
2025-04-29 19:38:11 +0500

    Update server.js
    cors header update

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
 const app = express();
-
+const corsOptions = {
+  origin: '*', // allow all
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 // Add this before your routes
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const allowedOrigin = 'http://localhost:5173/';
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
